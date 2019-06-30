6d66e3ba05536a80f8a28469e5125643c59078d2
EduardaHelena <dudinhahelenaa@gmail.com>
2019-06-30 01:07:08 -0300

    Moldura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { ListaDeCaronasComponent } from './lista-de-caronas/lista-de-caronas.component';
 import { CaronaComponent } from './carona/carona.component';
 import { CorridaComponent } from './corrida/corrida.component';
+import { MolduraDeCelularComponent } from './moldura-de-celular/moldura-de-celular.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CorridaComponent } from './corrida/corrida.component';
     PerfilComponent,
     ListaDeCaronasComponent,
     CaronaComponent,
-    CorridaComponent
+    CorridaComponent,
+    MolduraDeCelularComponent
   ],
   imports: [
     BrowserModule,
