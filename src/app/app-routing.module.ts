999376072f9f43f0dd44eb986a2d6a85c01c263b
Eduarda Helena <dudinhahelenaa@gmail.com>
2019-06-30 05:23:29 -0300

    incluindo rota para tela da corrida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { MapaComponent } from './mapa/mapa.component';
+import { CorridaComponent } from './corrida/corrida.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'mapa', component: MapaComponent },
-
+    { path: 'corrida', component: CorridaComponent },
   ];
 
 @NgModule({
