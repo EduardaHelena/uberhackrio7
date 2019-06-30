b607b9c4aa7038614d2989c6c8c1b523b9066280
EduardaHelena <dudinhahelenaa@gmail.com>
2019-06-30 07:32:29 -0300

    Esqueleto caronas

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -11,13 +11,7 @@ export class MapaComponent implements OnInit {
     {Titulo: 'teste0'},
     {Titulo: 'teste1'},
     {Titulo: 'teste2'},
-    {Titulo: 'teste3'},
-    {Titulo: 'teste4'},
-    {Titulo: 'teste5'},
-    {Titulo: 'teste6'},
-    {Titulo: 'teste7'},
-    {Titulo: 'teste8'},
-    {Titulo: 'teste9'},
+    {Titulo: 'teste3'}
   ];
   constructor() { }
 
